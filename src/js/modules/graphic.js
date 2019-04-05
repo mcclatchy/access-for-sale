f03ce647ea86f26bbed0aa7bd4c0c2a5344e5fbc
aaronalbright <aaron.albright38@gmail.com>
2019-04-05 13:20:34 -0400

    slightly less broken

--- src/js/modules/graphic.js
+++ src/js/modules/graphic.js
@@ -11,8 +11,8 @@ class Viz {
     const links = data.links.map(d => Object.create(d));
     const nodes = data.nodes.map(d => Object.create(d));
 
-    const nodeRadius = 12;
-    const linkDistance = 35;
+    const nodeRadius = 20;
+    const linkDistance = 40;
 
     const groupData = [
       { name: 'guest', mx: 1 },
@@ -32,19 +32,15 @@ class Viz {
           .forceLink(links)
           .distance(linkDistance)
           .id(d => d.id)
-          .strength(1)
       )
+      .force('charge', d3.forceManyBody().strength(-500).distanceMax(300))
       .force(
-        'charge',
-        d3
-          .forceManyBody()
-          .strength(-200)
-          // .distanceMax(300)
+        'collide',
+        d3.forceCollide().radius(d => getRadius(d.group))
       )
-      .force('collide', d3.forceCollide(nodeRadius))
       // .force('center', d3.forceCenter(width / 2, height / 2))
       .force('x', d3.forceX(width / 2))
-      .force('y', d3.forceY(height / 2));
+      .force('y', d3.forceY(height));
 
     const svg = d3
       .select('.svg')
@@ -70,8 +66,6 @@ class Viz {
 
     svg.call(handleZoom);
 
-    // handleZoom.scaleTo(g, 1.5);
-
     function zoomed() {
       g.attr('transform', d3.event.transform);
     }
@@ -85,10 +79,9 @@ class Viz {
     const line = linkG
       .append('path')
       .attr('id', d => d.index)
-      .attr('fill', '#333')
       .attr('stroke-opacity', 0.6)
       .attr('stroke', '#333')
-      .attr('stroke-width', '1');
+      .attr('stroke-width', '2');
 
     const lineText = linkG
       .append('text')
@@ -98,11 +91,11 @@ class Viz {
       .append('tspan')
       .attr(
         'style',
-        'text-anchor: middle; font: 12px sans-serif; user-select: none'
+        'text-anchor: middle; font: 22px sans-serif; user-select: none'
       )
       .attr('fill', '#333')
       .text('→')
-      .attr('dy', 4.35);
+      .attr('dy', 7.75);
 
     const node = g
       .append('g')
@@ -112,11 +105,11 @@ class Viz {
       .attr('data-name', d => d.id)
       .attr('class', d => `img-group ${d.group}`)
       .attr('fill', 'none')
-      .call(drag(simulation))
+      // .call(drag(simulation));
 
     const cir = node
       .append('circle')
-      .attr('r', d => groupCheck(d.group))
+      .attr('r', d => getRadius(d.group))
       .attr('stroke', '#333')
       .attr('stroke-width', 2);
 
@@ -129,10 +122,10 @@ class Viz {
         imgs[d.photo] !== undefined ? imgs[d.photo] : imgs['Unknown']
       )
       .attr('clip-path', d => `url(#${d.group}-clip)`)
-      .attr('width', d => groupCheck(d.group) * 2)
-      .attr('height', d => groupCheck(d.group) * 2)
-      .attr('x', d => groupCheck(d.group) * -1)
-      .attr('y', d => groupCheck(d.group) * -1);
+      .attr('width', d => getRadius(d.group) * 2)
+      .attr('height', d => getRadius(d.group) * 2)
+      .attr('x', d => getRadius(d.group) * -1)
+      .attr('y', d => getRadius(d.group) * -1);
 
     const tooltip = d3.select('.tooltip');
 
@@ -149,42 +142,28 @@ class Viz {
 
     node.on('touchend mouseleave', () => tooltip.style('visibility', 'hidden'));
 
-    let trump = node.filter(d => d.id == 'Donald J. Trump').datum();
+    let pres = node.filter(d => d.group == "president").datum();
     let tVict = node.filter(d => d.id == 'Trump Victory').datum();
     let tOrg = node.filter(d => d.id == 'The Trump Organization').datum();
 
-    simulation.on('tick', function() {
-      trump.fy = height / 8;
-      trump.fx = width / 2;
+    simulation.on('tick', () => {
+      pres.fy = height / 10;
+      pres.fx = width / 2;
 
-      tVict.fy = height / 4;
-      tVict.fx = (width / 2) + (nodeRadius * 6);
+      tVict.fy = height / 6;
+      tVict.fx = width / 2 + nodeRadius * 6;
 
-      tOrg.fy = height / 4;
-      tOrg.fx = (width / 2) - (nodeRadius * 6);
+      tOrg.fy = height / 6;
+      tOrg.fx = width / 2 - nodeRadius * 6;
 
       line.attr(
         'd',
-        d =>
-          `M ${computeMinMax(d.source.x, width)} ${computeMinMax(
-            d.source.y,
-            height
-          )} L ${computeMinMax(d.target.x, width)} ${computeMinMax(
-            d.target.y,
-            height
-          )}`
+        d => `M ${d.source.x} ${d.source.y} L ${d.target.x} ${d.target.y}`
       );
-      node.attr(
-        'transform',
-        d => `translate(${computeMinMax(d.x, width)},${computeMinMax(d.y, height)})`
-      );
+      node.attr('transform', d => `translate(${d.x},${d.y})`);
     });
 
-    function computeMinMax(value, reference) {
-      return Math.max(nodeRadius, Math.min(reference - nodeRadius, value))
-    }
-
-    function groupCheck(group) {
+    function getRadius(group) {
       let res;
       switch (group) {
         case 'guest':
@@ -219,14 +198,15 @@ function drag(simulation) {
 
   function dragended(d) {
     if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = d.x;
-    d.fy = d.y;
+    d.fx = null;
+    d.fy = null;
   }
 
-  return d3.drag()
-    .on("start", dragstarted)
-    .on("drag", dragged)
-    .on("end", dragended);
+  return d3
+    .drag()
+    .on('start', dragstarted)
+    .on('drag', dragged)
+    .on('end', dragended);
 }
 
 export { Viz };
